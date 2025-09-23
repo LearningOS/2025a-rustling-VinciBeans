fcaa0608416bc59a05b24e2772ca3650a227f6f5
VinciBeans <vincibeans@foxmail.com>
2025-09-23 03:01:55 -0700

    update

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,7 +11,7 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -35,7 +35,9 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    let mut cnt: usize = 0;
+    map.iter().filter(|(key, value0)| **value0 == value).map(|item| cnt += 1).collect::<Vec<_>>();
+    cnt
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +56,9 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    let mut cnt = 0;
+    collection.iter().map(|single_map| single_map.iter().filter(|(key, value0)| **value0 == value).map(|item| cnt += 1).collect::<Vec<_>>()).collect::<Vec<_>>();
+    cnt
 }
 
 #[cfg(test)]
